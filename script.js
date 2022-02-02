b84c5c793f010d3cd8b5fb327c719b4df9190900
Ladani Ankush <astarladani@gmail.com>
2022-02-02 12:42:39 +0530

    final code complted

--- script.js
+++ script.js
@@ -19,13 +19,46 @@ let textInput = document.querySelector("textarea");
 // Checking That It's Selecting Right Element Or Not?
 // console.log(textInput);
 
+// Selecting Div
+let Output = document.querySelector("#output");
+
+// For Checking
+// console.log(Output);
 
 
 // Added EventListener to Button
-button.addEventListener('click', () => {
-    let output = "";
-    output += textInput.value;
-    console.log(output);
-});
+// button.addEventListener('click', () => {
+//     let output = "";
+//     output += textInput.value;
+//     // console.log(output);
+//     Output.innerHTML = output;
+// });
+
+
+let serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+
+function getTranslatedURL(text){
+    return serverURL + '?' + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("Error Occured",error);
+    alert("Try After SomeTime");
+}
+
+function clickHandler(){
+    var inputText = textInput.value;
+    fetch(getTranslatedURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            Output.innerText = json.contents.translated
+        })
+        .catch(errorHandler)
+}
+
+
+button.addEventListener('click',clickHandler);
 
 
