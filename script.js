b1271bb6526c5f49f0b8d66d9ed2ab959936598e
Ladani Ankush <astarladani@gmail.com>
2022-02-02 10:47:26 +0530

    Alert And Prompt Function added

---
+++ script.js
@@ -0,0 +1,8 @@
+// For Output We use alert Function
+
+// alert("Alert Function is Working!");
+
+// For Input We use prompt Function
+
+let userName = prompt("What's Your Name? ");
+alert("Welcome " + userName);
